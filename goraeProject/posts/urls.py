a31672dd548d2633824f42f02b02e7fa524168f6
hong <trre1827151@gmail.com>
2023-08-02 13:00:21 +0900

    swagger추가, userInfoSerailizer 수정

--- goraeProject/posts/urls.py
+++ goraeProject/posts/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from .views import MessageViewSet
 
 
+
 message_router = DefaultRouter(trailing_slash=True)
 message_router.register('messages', MessageViewSet, basename='message')
 
